3e01a66b2dc4baf612e003ccd7e9846d487d4f1c
Lisa Prichodko <prichodko_lisaveta@mail.ru>
2023-03-30 18:57:50 +0000

    Revert "Merge branch 'Liza_P' into 'dev'"
    This reverts merge request !9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,7 +21,6 @@ namespace ConsoleApp1
                     case "Дисциплина": UI.Print(ClassCreator.Дисциплина()); break;
                     case "Группа": UI.Print(ClassCreator.Группа()); break;
                     case "Специальность": UI.Print(ClassCreator.Специальность()); break;
-                    case "Должность": UI.Print(ClassCreator.Должность()); break;
                     case "Пара": UI.Print(ClassCreator.Пара()); break;
                     default: Console.WriteLine("Такой класс не реализован"); break;
                 }
